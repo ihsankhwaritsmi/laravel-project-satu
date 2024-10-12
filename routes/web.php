c01eed3f7609ee0c092d2e0b6c1b01f362ef69da
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2024-10-12 15:23:17 +0700

    update: Searching

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     // $posts = Post::with(['author','category'])->latest()->get();
 
-    $posts = Post::latest()->get();
-    return view('posts',['title' => 'Blog', 'posts' => $posts]);
+    // dump(request('search'));
+
+    return view('posts',['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category','author' ]))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
